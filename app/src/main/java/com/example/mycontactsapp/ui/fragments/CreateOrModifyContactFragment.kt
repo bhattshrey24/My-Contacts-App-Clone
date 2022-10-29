5576f44eec32f93d1376e5005d92b384e3a4d7d7
Shrey <bhatt.shrey1224@gmail.com>
2022-10-29 17:49:52 +0530

    working version

--- app/src/main/java/com/example/mycontactsapp/ui/fragments/CreateOrModifyContactFragment.kt
+++ app/src/main/java/com/example/mycontactsapp/ui/fragments/CreateOrModifyContactFragment.kt
@@ -35,16 +35,6 @@ class CreateOrModifyContactFragment : Fragment() {
         FragmentCreateOrModifyContactBinding.inflate(layoutInflater, null, false)
     }
 
-    private var hmOfNumbersEditTexts =
-        mutableMapOf<PhoneTypes, TextInputLayout>() // This will hold the
-
-//todo Must
-// must check if et is empty then discard it
-
-    // reference to edit texts since I'm programmatically making them based on number of emails or phone numbers
-    // user has for a particular contact
-    private var hmOfEmailsEditTexts = mutableMapOf<EmailTypes, TextInputLayout>()
-
     private val args: CreateOrModifyContactFragmentArgs by navArgs()
     private val sharedViewModel: ListOfContactsViewModel by activityViewModels()
 
@@ -65,18 +55,7 @@ class CreateOrModifyContactFragment : Fragment() {
 
         setUpListeners(isEdit, contactDetails)
 
-        setUpNewListeners()
-
         return binding.root
-    }
-
-    private fun setUpNewListeners() {
-        binding.myBtnForAddingNumber.setOnClickListener {
-            addViewToPhoneLL()
-        }
-        binding.myBtnForAddingEmail.setOnClickListener {
-            addViewToEmailLL()
-        }
     }
 
     private fun setUpListeners(isEdit: Boolean?, contactDetails: Contact?) {
@@ -92,6 +71,13 @@ class CreateOrModifyContactFragment : Fragment() {
                 )
             }
         }
+
+        binding.myBtnForAddingNumber.setOnClickListener {
+            addViewToPhoneLL()
+        }
+        binding.myBtnForAddingEmail.setOnClickListener {
+            addViewToEmailLL()
+        }
     }
 
     private fun setUpUi(isEdit: Boolean?): Contact? {
@@ -105,15 +91,6 @@ class CreateOrModifyContactFragment : Fragment() {
             setUpUiForCreateNewContact()
         }
         return null
-    }
-
-    private fun getCategoryIndexInSpinner(spinner: Spinner, category: String): Int {
-        for (i in 0 until spinner.count) {
-            if (spinner.getItemAtPosition(i).toString().equals(category)) {
-                return i
-            }
-        }
-        return 0
     }
 
     private fun setUpUiForCreateNewContact() {
@@ -140,6 +117,120 @@ class CreateOrModifyContactFragment : Fragment() {
                 setViewWithDataToEmailLL(category, email.value)
             }
         }
+    }
+
+    private fun updateValues(oldContactDetails: Contact?, updatedContactName: String) {
+        val updatedHashMapForNum = mutableMapOf<PhoneTypes, String>()
+        val updatedHashMapForEmail = mutableMapOf<EmailTypes, String>()
+
+        val countForNum = binding.parentLLayoutForNumber.childCount
+        var viewForNum: View?
+        for (i in 0 until countForNum) {
+            viewForNum = binding.parentLLayoutForNumber.getChildAt(i)
+            val phoneNumber: EditText = viewForNum.findViewById(R.id.et_phone)
+            val phoneType: Spinner = viewForNum.findViewById(R.id.phoneTypeSpinner)
+            val numberEntered = phoneNumber.text.toString()
+            if (numberEntered.isEmpty()) {
+                continue
+            }
+            val category = phoneType.selectedItem as String
+            updatedHashMapForNum.put(getEnumPhoneType(category), numberEntered)
+        }
+
+        val countForEmail = binding.parentLLayoutForEmail.childCount
+        var viewForEmail: View?
+        for (i in 0 until countForEmail) {
+            viewForEmail = binding.parentLLayoutForEmail.getChildAt(i)
+            val emailAddress: EditText = viewForEmail.findViewById(R.id.et_email)
+            val emailType: Spinner = viewForEmail.findViewById(R.id.emailTypeSpinner)
+            val emailEntered = emailAddress.text.toString()
+            if (emailEntered.isEmpty()){
+                continue
+            }
+            val category = emailType.selectedItem as String
+            updatedHashMapForEmail.put(getEnumEmailType(category), emailEntered)
+        }
+
+        // updating the list in shared viewModel
+        updateContactList(
+            oldContactDetails,
+            updatedContactName,
+            updatedHashMapForNum,
+            updatedHashMapForEmail
+        )
+
+        findNavController().popBackStack()
+    }
+
+    private fun updateContactList(
+        oldContactDetails: Contact?,
+        updatedContactName: String,
+        updatedHashMapForNum: MutableMap<PhoneTypes, String>,
+        updatedHashMapForEmail: MutableMap<EmailTypes, String>
+    ) {
+        oldContactDetails?.let {
+            it.emails = updatedHashMapForEmail
+            it.numbers = updatedHashMapForNum
+            it.name = updatedContactName
+            sharedViewModel.updateContact(it)
+            sharedViewModel.listOfContact.value?.sortWith(ListSortComparator()) // sorting because it's
+            // possible that user might changed the name of contact
+        }
+    }
+
+    private fun createNewContact(nameOfContact: String) {
+        val updatedHashMapForNum = mutableMapOf<PhoneTypes, String>()
+        val updatedHashMapForEmail = mutableMapOf<EmailTypes, String>()
+        val countForNum = binding.parentLLayoutForNumber.childCount
+        var viewForNum: View?
+        for (i in 0 until countForNum) {
+            viewForNum = binding.parentLLayoutForNumber.getChildAt(i)
+            val phoneNumber: EditText = viewForNum.findViewById(R.id.et_phone)
+            val phoneType: Spinner = viewForNum.findViewById(R.id.phoneTypeSpinner)
+            val numberEntered = phoneNumber.text.toString()
+            if (numberEntered.isEmpty()) {
+                continue
+            }
+            val category = phoneType.selectedItem as String
+            updatedHashMapForNum.put(getEnumPhoneType(category), numberEntered)
+        }
+
+        val countForEmail = binding.parentLLayoutForEmail.childCount
+        var viewForEmail: View?
+        for (i in 0 until countForEmail) {
+            viewForEmail = binding.parentLLayoutForEmail.getChildAt(i)
+            val emailAddress: EditText = viewForEmail.findViewById(R.id.et_email)
+            val emailType: Spinner = viewForEmail.findViewById(R.id.emailTypeSpinner)
+            val emailEntered = emailAddress.text.toString()
+            if (emailEntered.isEmpty()) {
+                continue
+            }
+            val category = emailType.selectedItem as String
+            updatedHashMapForEmail.put(getEnumEmailType(category), emailEntered)
+        }
+
+
+        val newContact =
+            Contact(
+                null,
+                nameOfContact,
+                updatedHashMapForNum,
+                updatedHashMapForEmail
+            )
+
+        sharedViewModel.insertContact(newContact)
+        sharedViewModel.listOfContact.value?.sortWith(ListSortComparator())
+
+        findNavController().popBackStack()
+    }
+
+    private fun getCategoryIndexInSpinner(spinner: Spinner, category: String): Int {
+        for (i in 0 until spinner.count) {
+            if (spinner.getItemAtPosition(i).toString().equals(category)) {
+                return i
+            }
+        }
+        return 0
     }
 
     private fun setViewWithDataToPhoneLL(category: String, number: String) {
@@ -178,205 +269,34 @@ class CreateOrModifyContactFragment : Fragment() {
         binding.parentLLayoutForEmail.addView(inflater, binding.parentLLayoutForEmail.childCount)
     }
 
-    private fun updateValues(oldContactDetails: Contact?, updatedContactName: String) {
-        if (!isValidated()) {
-            return
-        }
-
-        val updatedHashMapForNum = mutableMapOf<PhoneTypes, String>()
-        val updatedHashMapForEmail = mutableMapOf<EmailTypes, String>()
-
-        for (num in hmOfNumbersEditTexts) {
-            updatedHashMapForNum.put(num.key, num.value.editText?.text?.trim().toString())
-        }
-        for (email in hmOfEmailsEditTexts) {
-            updatedHashMapForEmail.put(email.key, email.value.editText?.text?.trim().toString())
-        }
-
-        // updating the list in shared viewModel
-        updateContactList(
-            oldContactDetails,
-            updatedContactName,
-            updatedHashMapForNum,
-            updatedHashMapForEmail
-        )
-
-        findNavController().popBackStack()
-    }
-
-    private fun updateContactList(
-        oldContactDetails: Contact?,
-        updatedContactName: String,
-        updatedHashMapForNum: MutableMap<PhoneTypes, String>,
-        updatedHashMapForEmail: MutableMap<EmailTypes, String>
-    ) {
-        oldContactDetails?.let {
-            it.emails = updatedHashMapForEmail
-            it.numbers = updatedHashMapForNum
-            it.name = updatedContactName
-            sharedViewModel.updateContact(it)
-            sharedViewModel.listOfContact.value?.sortWith(ListSortComparator()) // sorting because it's
-            // possible that user might changed the name of contact
-        }
-    }
-
-
-    private fun createNewContact(nameOfContact: String) {
-        if (!isValidated()) {
-            return
-        }
-        for (numb in hmOfNumbersEditTexts.keys) {
-            val number = hmOfNumbersEditTexts.get(numb)?.editText?.text?.toString()?.trim()
-            val newContact =
-                Contact(
-                    null, // setting null because this
-                    // will tell me whether the contact was there in Android DB or
-                    // not
-                    nameOfContact,
-                    mutableMapOf(PhoneTypes.Mobile to number!!),
-                    null
-                )
-            sharedViewModel.insertContact(newContact)
-        }
-        sharedViewModel.listOfContact.value?.sortWith(ListSortComparator())
-        findNavController().popBackStack()
-    }
-
-    private fun isValidated(): Boolean {
-        val nameEt = binding.nameOfPersonET
-        if (nameEt.text.isBlank()) {
-            nameEt.error = "Name cannot be empty"
-            return false
-        }
-        if (hmOfNumbersEditTexts.isEmpty()) {
-            for (numbersEt in hmOfNumbersEditTexts) {
-                if (numbersEt.value.editText?.text?.toString()?.isBlank() == true) {
-                    numbersEt.value.error = "Number cannot be empty"
-                    return false
-                }
+    private fun getEnumPhoneType(category: String): PhoneTypes {
+        return when (category) {
+            "Mobile" -> {
+                PhoneTypes.Mobile
+            }
+            "Work" -> {
+                PhoneTypes.Work
+            }
+            "Home" -> {
+                PhoneTypes.Home
+            }
+            else -> {
+                PhoneTypes.Mobile
             }
         }
-        if (hmOfEmailsEditTexts.isEmpty()) {
-            for (emailsEt in hmOfEmailsEditTexts) {
-                if (emailsEt.value.editText?.text?.toString()?.isBlank() == true) {
-                    emailsEt.value.error = "Email address cannot be empty"
-                    return false
-                }
-            }
-        }
-        return true
     }
 
+    private fun getEnumEmailType(category: String): EmailTypes {
+        return when (category) {
+            "Work" -> {
+                EmailTypes.Work
+            }
+            "Home" -> {
+                EmailTypes.Home
+            }
+            else -> {
+                EmailTypes.Home
+            }
+        }
+    }
 }
-
-
-//private fun setUpListenerForFab() {
-//    fabForNumber?.let {
-//        it.setOnClickListener {
-//            val et = makeEditText("Work", "", Type.Phone)
-//            binding.linearLayoutNumber.addView(et)
-//            hmOfNumbersEditTexts.put(PhoneTypes.Work, et)
-//        }
-//    }
-//    fabForEmail?.let {
-//        it.setOnClickListener {
-//            val et = makeEditText("Home", "", Type.Phone)
-//            binding.linearLayoutEmail.addView(et)
-//            hmOfEmailsEditTexts.put(EmailTypes.Home, et)
-//        }
-//    }
-//}
-
-//private fun makeFAB(): FloatingActionButton {
-//    return FloatingActionButton(requireContext()).apply {
-//        setPadding(20, 20, 20, 0)
-//        layoutParams = LinearLayout.LayoutParams(
-//            ViewGroup.LayoutParams.WRAP_CONTENT,// width
-//            ViewGroup.LayoutParams.WRAP_CONTENT// height
-//        ).also {
-//            it.setMargins(20, 5, 20, 5)
-//        }
-//        // size=FloatingActionButton.SIZE_MINI
-//        setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_add))
-//    }
-//}
-
-
-//private fun makeEditText(hint: String, fetchedData: String, type: Type): TextInputLayout {
-//    val textInputLayout = TextInputLayout(requireContext()) // this makes it possible
-//    // to have floating hint in edit text . It is wrapper class that can wrap Edit text
-//    val editText = EditText(requireContext())
-//    editText.apply {
-//        setHint(hint)
-//        layoutParams = LinearLayout.LayoutParams(
-//            ViewGroup.LayoutParams.MATCH_PARENT,// width
-//            200// height
-//        ).also {
-//            it.setMargins(30)
-//        }
-//        setPadding(20, 20, 20, 0)
-//        setBackgroundResource(R.drawable.edit_text_border)
-//        setText(fetchedData)
-//        inputType = when (type) {
-//            Type.Phone -> {
-//                InputType.TYPE_CLASS_NUMBER
-//            }
-//            Type.Email -> {
-//                InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
-//            }
-//        }
-//    }
-//    textInputLayout.addView(editText)
-//    return textInputLayout
-//}
-
-//private fun setUpUiForEditScreen(contactDetails: Contact?) {
-//    binding.createOrEditTV.text = "Edit Contact"
-//    binding.nameOfPersonET.setText(contactDetails?.name)
-//    binding.eocSubmitButton.text = "Edit"
-//    contactDetails?.numbers?.let { numbers ->
-//        for (number in numbers) {
-//            var hint: String
-//            var type: PhoneTypes
-//            when (number.key.codeOfType) {
-//                PhoneTypes.Mobile.codeOfType -> {
-//                    hint = PhoneTypes.Mobile.nameOfType
-//                    type = PhoneTypes.Mobile
-//                }
-//                PhoneTypes.Home.codeOfType -> {
-//                    hint = PhoneTypes.Home.nameOfType
-//                    type = PhoneTypes.Home
-//                }
-//                PhoneTypes.Work.codeOfType -> {
-//                    hint = PhoneTypes.Work.nameOfType
-//                    type = PhoneTypes.Work
-//                }
-//                else -> {
-//                    hint = PhoneTypes.Mobile.nameOfType
-//                    type = PhoneTypes.Mobile
-//                }
-//            }
-//        }
-//    }
-//    contactDetails?.emails?.let { emails ->
-//        for (email in emails) {
-//            var hint: String
-//            var type: EmailTypes
-//            when (email.key.codeOfType) {
-//                EmailTypes.Work.codeOfType -> {
-//                    hint = EmailTypes.Work.nameOfType
-//                    type = EmailTypes.Work
-//                }
-//                EmailTypes.Home.codeOfType -> {
-//                    hint = EmailTypes.Home.nameOfType
-//                    type = EmailTypes.Home
-//                }
-//                else -> {
-//                    hint = EmailTypes.Home.nameOfType
-//                    type = EmailTypes.Home
-//                }
-//            }
-//
-//        }
-//    }
-//}
