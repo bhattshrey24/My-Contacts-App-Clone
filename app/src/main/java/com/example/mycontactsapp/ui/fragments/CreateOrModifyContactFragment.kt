22feae6fd2a29d0743d415e345e6015faf37a6f7
Shrey <bhatt.shrey1224@gmail.com>
2022-10-31 12:59:12 +0530

    delete field feature added

--- app/src/main/java/com/example/mycontactsapp/ui/fragments/CreateOrModifyContactFragment.kt
+++ app/src/main/java/com/example/mycontactsapp/ui/fragments/CreateOrModifyContactFragment.kt
@@ -8,10 +8,8 @@ import android.text.InputType
 import android.util.Log
 
 import android.view.*
+import android.widget.*
 import androidx.fragment.app.Fragment
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.Spinner
 import androidx.core.content.ContextCompat
 
 import androidx.core.view.setMargins
@@ -131,7 +129,8 @@ class CreateOrModifyContactFragment : Fragment() {
             val phoneType: Spinner = viewForNum.findViewById(R.id.phoneTypeSpinner)
             val numberEntered = phoneNumber.text.toString()
             if (numberEntered.isEmpty()) {
-                continue
+                phoneNumber.setError("Enter a number")
+                return
             }
             val category = phoneType.selectedItem as String
             updatedHashMapForNum.put(getEnumPhoneType(category), numberEntered)
@@ -144,8 +143,13 @@ class CreateOrModifyContactFragment : Fragment() {
             val emailAddress: EditText = viewForEmail.findViewById(R.id.et_email)
             val emailType: Spinner = viewForEmail.findViewById(R.id.emailTypeSpinner)
             val emailEntered = emailAddress.text.toString()
-            if (emailEntered.isEmpty()){
-                continue
+            if (emailEntered.isEmpty()) {
+                emailAddress.setError("Enter an email address")
+                return
+            }
+            if (!regExToCheckEmail(emailEntered)){
+                emailAddress.setError("Enter a valid email address")
+                return
             }
             val category = emailType.selectedItem as String
             updatedHashMapForEmail.put(getEnumEmailType(category), emailEntered)
@@ -160,6 +164,10 @@ class CreateOrModifyContactFragment : Fragment() {
         )
 
         findNavController().popBackStack()
+    }
+
+    private fun regExToCheckEmail(str: String): Boolean {
+        return str.matches(Regex("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"))
     }
 
     private fun updateContactList(
@@ -189,7 +197,8 @@ class CreateOrModifyContactFragment : Fragment() {
             val phoneType: Spinner = viewForNum.findViewById(R.id.phoneTypeSpinner)
             val numberEntered = phoneNumber.text.toString()
             if (numberEntered.isEmpty()) {
-                continue
+                phoneNumber.setError("Enter a number")
+                return
             }
             val category = phoneType.selectedItem as String
             updatedHashMapForNum.put(getEnumPhoneType(category), numberEntered)
@@ -203,7 +212,12 @@ class CreateOrModifyContactFragment : Fragment() {
             val emailType: Spinner = viewForEmail.findViewById(R.id.emailTypeSpinner)
             val emailEntered = emailAddress.text.toString()
             if (emailEntered.isEmpty()) {
-                continue
+                emailAddress.setError("Enter an email address")
+                return
+            }
+            if (!regExToCheckEmail(emailEntered)){
+                emailAddress.setError("Enter a valid email address")
+                return
             }
             val category = emailType.selectedItem as String
             updatedHashMapForEmail.put(getEnumEmailType(category), emailEntered)
@@ -235,38 +249,74 @@ class CreateOrModifyContactFragment : Fragment() {
 
     private fun setViewWithDataToPhoneLL(category: String, number: String) {
         addViewToPhoneLL()
-        val countForNum = binding.parentLLayoutForNumber.childCount
-        var view: View? = binding.parentLLayoutForNumber.getChildAt(countForNum - 1)
+        val countForNum = binding.parentLLayoutForNumber.childCount - 1
+        var view: View? = binding.parentLLayoutForNumber.getChildAt(countForNum)
         view?.let {
             val phoneNumber: EditText = view.findViewById(R.id.et_phone)
             phoneNumber.setText(number)
             val phoneType: Spinner = it.findViewById(R.id.phoneTypeSpinner)
             val selectionIdx = getCategoryIndexInSpinner(phoneType, category)
             phoneType.setSelection(selectionIdx)
+
+            val delBtn: FloatingActionButton = view.findViewById(R.id.deleteBtnPhone)
+            delBtn.setOnClickListener {
+                if (binding.parentLLayoutForNumber.childCount <= 1) {
+                    Toast.makeText(context , "Contact should atleast have 1 number",Toast.LENGTH_SHORT).show()
+                }else{
+                    binding.parentLLayoutForNumber.removeView(view)
+                }
+            }
         }
     }
 
     private fun setViewWithDataToEmailLL(category: String, emailAddress: String) {
         addViewToEmailLL()
-        val countForNum = binding.parentLLayoutForEmail.childCount
-        var view: View? = binding.parentLLayoutForEmail.getChildAt(countForNum - 1)
+        val countForEmail = binding.parentLLayoutForEmail.childCount - 1
+        var view: View? = binding.parentLLayoutForEmail.getChildAt(countForEmail)
         view?.let {
             val email: EditText = view.findViewById(R.id.et_email)
             email.setText(emailAddress)
             val emailType: Spinner = it.findViewById(R.id.emailTypeSpinner)
             val selectionIdx = getCategoryIndexInSpinner(emailType, category)
             emailType.setSelection(selectionIdx)
+
+            val delBtn: FloatingActionButton = view.findViewById(R.id.deleteBtnEmail)
+            delBtn.setOnClickListener {
+                binding.parentLLayoutForEmail.removeView(view)
+            }
         }
     }
 
     private fun addViewToPhoneLL() {
         val inflater = LayoutInflater.from(context).inflate(R.layout.child_layout_phone, null)
         binding.parentLLayoutForNumber.addView(inflater, binding.parentLLayoutForNumber.childCount)
+
+        val countForNum = binding.parentLLayoutForNumber.childCount - 1
+        var view: View? = binding.parentLLayoutForNumber.getChildAt(countForNum)
+        view?.let {
+            val delBtn: FloatingActionButton = view.findViewById(R.id.deleteBtnPhone)
+            delBtn.setOnClickListener {
+                if (binding.parentLLayoutForNumber.childCount <= 1) {
+                    Toast.makeText(context , "Contact should atleast have 1 number",Toast.LENGTH_SHORT).show()
+                }else{
+                    binding.parentLLayoutForNumber.removeView(view)
+                }
+            }
+        }
     }
 
     private fun addViewToEmailLL() {
         val inflater = LayoutInflater.from(context).inflate(R.layout.child_layout_email, null)
         binding.parentLLayoutForEmail.addView(inflater, binding.parentLLayoutForEmail.childCount)
+
+        val countForEmail = binding.parentLLayoutForEmail.childCount - 1
+        val view: View? = binding.parentLLayoutForEmail.getChildAt(countForEmail)
+        view?.let {
+            val delBtn: FloatingActionButton = view.findViewById(R.id.deleteBtnEmail)
+            delBtn.setOnClickListener {
+                binding.parentLLayoutForEmail.removeView(view)
+            }
+        }
     }
 
     private fun getEnumPhoneType(category: String): PhoneTypes {
